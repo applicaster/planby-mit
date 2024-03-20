9a5f9a9c5bff0ce505e653c6b99549511f6c321b
aa-appli <121756132+aa-appli@users.noreply.github.com>
2024-03-20 13:28:18 +0200

    Update Loader.tsx

--- src/Epg/components/Loader.tsx
+++ src/Epg/components/Loader.tsx
@@ -1,34 +1,9 @@
 import * as React from 'react';
-// import { theme } from '../theme/theme';
 
 // Import styles
 import { LoaderStyled } from '../styles';
 
-// const { Box, Shape } = LoaderStyled;
 const { Box } = LoaderStyled;
-
-// const Element = ({
-//   width,
-//   color,
-//   animate,
-//   marginRight,
-//   transition,
-// }: {
-//   width: number;
-//   color: string;
-//   transition: { duration: number; ease?: string; delay?: number };
-//   animate: { right: string[] };
-//   marginRight?: number;
-// }) => (
-//   <Shape
-//     className="SHAPE"
-//     width={width}
-//     color={color}
-//     animate={animate}
-//     transition={transition}
-//     marginRight={marginRight}
-//   />
-// );
 
 export function Loader({
   loadingSkeletonItemHeight,
@@ -42,73 +17,6 @@ export function Loader({
           loadingSkeletonItemHeight={loadingSkeletonItemHeight}
         />
       </div>
-
-      {/* <div
-          style={{ display: 'flex', justifyContent: 'end', marginBottom: 16 }}
-        >
-          <Element
-            animate={{
-              right: ['0px', '75px', '0px'],
-            }}
-            transition={{
-              duration: 1,
-            }}
-            width={217}
-            color={theme.loader.teal}
-            marginRight={10}
-          />
-          <Element
-            width={122}
-            color={theme.loader.purple}
-            animate={{
-              right: ['0px', '50px', '0px'],
-            }}
-            transition={{
-              duration: 1,
-              delay: 0.02,
-            }}
-          />
-        </div>
-        <div
-          style={{ display: 'flex', justifyContent: 'end', marginBottom: 16 }}
-        >
-          <Element
-            width={267}
-            color={theme.loader.pink}
-            animate={{
-              right: ['0px', '70px', '0px'],
-            }}
-            transition={{
-              delay: 0.5,
-              duration: 1,
-            }}
-          />
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'end' }}>
-          <Element
-            width={217}
-            color={theme.loader.purple}
-            marginRight={10}
-            animate={{
-              right: ['0px', '95px', '0px'],
-            }}
-            transition={{
-              delay: 0.3,
-              duration: 1,
-            }}
-          />
-          <Element
-            width={80}
-            color={theme.loader.teal}
-            animate={{
-              right: ['0px', '70px', '0px'],
-            }}
-            transition={{
-              duration: 1,
-              delay: 0.33,
-            }}
-          />
-        </div> */}
     </Box>
   );
 }
@@ -125,8 +33,9 @@ function EPGLoadingSkeleton({
     },
     programSkeleton: {
       width: '100%',
-      height: `${loadingSkeletonItemHeight}px`,
-      // height: `150px`,
+      height: loadingSkeletonItemHeight
+        ? `${loadingSkeletonItemHeight}px`
+        : `80px`,
       backgroundColor: '#eee',
       marginBottom: '10px',
       animation: 'skeleton-loading 1.5s ease-in-out infinite alternate',
